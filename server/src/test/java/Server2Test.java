6c27213b730fbd75c84b0a089f2f302887f38f95
Sebastien Kerbrat <sebastien@piktiv.se>
2017-03-22 11:38:46 +0100

    Tests pass

---
+++ server/src/test/java/Server2Test.java
@@ -0,0 +1,20 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Created by Leonardo on 2017-03-22.
+ */
+public class Server2Test {
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @Test
+    public void main() throws Exception {
+        assertTrue(true);
+    }
+
+}
---
+++ server/src/test/java/ServerTest.java
@@ -0,0 +1,23 @@
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Created by Leonardo on 2017-03-22.
+ */
+@RunWith(JUnit4.class)
+public class ServerTest {
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @Test
+    public void main() throws Exception {
+assertTrue(true);
+    }
+
+}
